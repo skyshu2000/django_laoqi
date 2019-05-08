4051541d400b8d8f9b5b8d4f746d1dd73fda22f4
skyshu2000 <skyshu2000@live.com>
2019-05-08 13:52:41 +0800

    Fix issue#1 and add corresponding test case.

--- mysite/blog/tests.py
+++ mysite/blog/tests.py
@@ -1,3 +1,41 @@
 from django.test import TestCase
+from django.urls import reverse
+from django.contrib.auth.models import User
+from .models import BlogArticles
 
-# Create your tests here.
+class BlogDetailViewTest(TestCase):    
+    def setUp(self):
+        # Create one user
+        user1 = User.objects.create_user(username='test1', password='!QAZ1qaz', is_superuser=True)
+        user1.save()
+        
+        # Create one blog article
+        blog_article_1 = BlogArticles.objects.create(
+            title = "title 1",
+            author = user1,
+            body = "body 1",
+        )
+        blog_article_1.save()
+    
+    def test_blog_list(self):
+        response = self.client.get(reverse('blog:blog_title'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_blog_with_exist_id(self):
+        """
+        if the blog id exists, an appropriate message is displayed.
+        """
+        url = reverse('blog:blog_article', kwargs={'article_id':1})
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        
+    
+    def test_blog_with_not_exist_id(self):
+        """
+        if the blog id does NOT exist, an appropriate message is displayed.
+        """
+        url = reverse('blog:blog_article', kwargs={'article_id':202})        
+        response = self.client.get(url)       
+        self.assertEqual(response.status_code, 404)
+
+    
