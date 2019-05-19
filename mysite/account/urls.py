0d8b320cab24177b14ccebb684b5c5a8aebe26ad
skyshu2000 <skyshu2000@live.com>
2019-05-19 17:24:04 +0800

    Add feature of built-in logout

--- mysite/account/urls.py
+++ mysite/account/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, LogoutView
 from . import views
 
 app_name = "account"
@@ -7,6 +7,10 @@ app_name = "account"
 urlpatterns = [
     # 自定义登录方法的路由
     path('login/', views.user_login, name="user_login"),
+
     # 使用内置的LoginView类处理登录的路由
     path('built-in-login/', LoginView.as_view(template_name='built_in_login.html'), name='built_in_login'),
+
+    # 使用内置的LogoutView类处理登出的路由
+    path('built-in-logout/', LogoutView.as_view(next_page='/blog/'), name='built_in_logout'),    
 ]
