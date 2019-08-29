a5ba561c62a540dc3cb6e1921c62c3d9c07d6471
skyshu2000 <skyshu2000@live.com>
2019-08-29 14:05:55 +0800

    New feature: add latest N most commented articles template tag.

--- mysite/article/templatetags/article_tags.py
+++ mysite/article/templatetags/article_tags.py
@@ -16,3 +16,11 @@ def author_total_articles(user):
 def latest_articles(n=5):
     latest_articles = ArticlePost.objects.order_by("-created")[:n]
     return {"latest_articles": latest_articles}
+
+from django.db.models import Count
+@register.simple_tag
+def most_commented_articles(n=3):
+    # 参考：https://docs.djangoproject.com/zh-hans/2.2/topics/db/aggregation/#generating-aggregates-for-each-item-in-a-queryset
+    # 使用 annotate() 函数为 QuerySet 生产特定指标的聚合
+
+    return ArticlePost.objects.annotate(total_comments=Count('comments')).order_by('-total_comments')[:n]
